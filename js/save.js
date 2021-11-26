e43be5df471f2fd8e9e72f74503f1a5c6cd32df4
Kniffen <knifftech@gmail.com>
2021-11-26 14:00:17 +0100

    Update save.js

--- js/save.js
+++ js/save.js
@@ -1,6 +1,8 @@
 import { playlists } from './main.js'
 
 export default function save() {
+  if (!localStorage.privacy) return
+  
   console.log('Saving playlists')
   localStorage.playlists = JSON.stringify(playlists)
 }
